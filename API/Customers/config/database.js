4b05b8a65b2aaab2a53cc5ab6e4a816abb55af0f
nitikornchumnankul <nitikornchumnankul@gmail.com>
2020-02-07 02:30:53 +0700

    folder

--- API/Customers/config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports ={
-    database:'mongodb://0.0.0.0:27017/customer'
-}
--- API/Customers/models/customer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const customerSchema = new mongoose.Schema({
-    name:String
-});
-
-var customer =module.exports = mongoose.model('customer',customerSchema);
-module.exports = customer;
-
-//Metthod CRUD
