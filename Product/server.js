8697f38a69e9deda6cf2b31847f16947cea6ba88
nitikornchumnankul <nitikornchumnankul@gmail.com>
2020-02-06 21:31:07 +0700

    add product

--- Product/server.js
+++ Product/server.js
@@ -2,26 +2,31 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/database');
-const cors = require('cors');
-const port = process.env.PORT || 8080;
-
+const cors = require('cors');  
+const dotenv = require('dotenv');
 const product = require('./routes/product');
-
+const routers = require('./routes/product')
+dotenv.config();
 app.use('/product',product);
 app.use(cors());
+
 app.use(bodyParser.json());
+//parse application/json
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 
 mongoose.connection.on('error',(err)=>{
     console.log('Database error' + err);
 })
 
-mongoose.connect(config.database);
+mongoose.connect(process.env.DATABASE);
 mongoose.connection.on('connected',()=>{
-    console.log('Connected to database' + config.database);
+    console.log('Connected to database ' + process.env.DATABASE);
 });
 
-app.listen(port,() =>{
-    console.log('Server start on port')
+app.listen(process.env.PORT,() =>{
+    console.log('Server start on port ' + process.env.PORT)
 });
