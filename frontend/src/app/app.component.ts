a00060f1278961656e618e4e0b4ed30990c4b77e
Nitikorn <nitikornchumnankul@gmail.com>
2020-04-18 21:43:26 +0700

    edit status nginx config

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+interface Status{
+  status:string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+
   title = 'frontend';
+  status_json :Status = {
+    status:"OK"
+  }
+
+  constructor(private httpClient:HttpClient,header:HttpHeaders){
+    
+  }
+  
+  ngOnInit() {
+    this.httpClient.get('http://0.0.0.0:80/',).subscribe(res =>{
+      console.log(this.status_json)
+    })
+   console.log(this.status_json)
+  }
+
+
+
 }
+
+
+
